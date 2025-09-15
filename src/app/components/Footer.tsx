1bd8e74f18c8bba5c8bc08c1b58aa6cc07d30f1c
webbs445 <web@best-solution.ae>
2025-09-15 16:59:54 +0400

    Fix:  updated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -73,7 +73,7 @@ export default function FooterSection() {
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
-                allowFullScreen=""
+               allowFullScreen={true}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
