0051a0637a0cfe9db73def170c56ac56f646cd6c
webbs445 <web@best-solution.ae>
2025-09-16 16:10:54 +0400

    Fix: blog  updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Fintech Launchpad",
+  title: "Grovoz - Marketing Agency",
   description: "Modern Fintech card & payments platform landing page starter (Next.js + Tailwind)."
 };
 
